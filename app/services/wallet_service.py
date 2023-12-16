57a7d45999f084b37ee60687fd62577be999068b
Adedamola Ajasa <96835056+damoxify@users.noreply.github.com>
2023-12-16 18:28:06 +0100

    feat: Introduce WalletService for user wallet operations

--- app/services/wallet_service.py
+++ app/services/wallet_service.py
@@ -1,17 +1,32 @@
 from app import db
-from app.models.wallet import Wallet_account
+from app.models.wallet import Wallet
 
 class WalletService:
     @staticmethod
     def create_wallet(user_id):
 
-        new_wallet = Wallet_account(user_id=user_id)
-        db.session.add(new_wallet)
-        db.session.commit()
-        return new_wallet
+        try:
+            if not user_id:
+                raise ValueError("Invalid user_id for creating a wallet")
+
+            new_wallet = Wallet(user_id=user_id)
+            db.session.add(new_wallet)
+            db.session.commit()
+
+            return new_wallet
+
+        except Exception as e:
+            db.session.rollback()
+            raise e
 
     @staticmethod
     def get_wallet_by_user_id(user_id):
-    
-        return Wallet_account.query.filter_by(user_id=user_id).first()
+ 
+        try:
+            if not user_id:
+                raise ValueError("Invalid user_id for retrieving a wallet")
 
+            return Wallet.query.filter_by(user_id=user_id).first()
+
+        except Exception as e:
+            raise e
