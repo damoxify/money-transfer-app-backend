e203bbd739eee7d7bc3506a615ccdfdc368d209c
Adedamola Ajasa <96835056+damoxify@users.noreply.github.com>
2023-12-16 17:53:17 +0100

    chore: Configure PostgreSQL database connection

---
+++ app/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/money_transfer_app_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ app/run.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
