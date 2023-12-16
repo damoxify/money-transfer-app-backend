885cce1c1c1a171aa47b22c741f68f31f768641e
Adedamola Ajasa <96835056+damoxify@users.noreply.github.com>
2023-12-16 18:30:15 +0100

    feat: Introduce TransactionService for handling financial transactions

--- app/services/transaction_service.py
+++ app/services/transaction_service.py
@@ -5,16 +5,33 @@ from wallet_service import WalletService
 class TransactionService:
     @staticmethod
     def create_transaction(sender_wallet_id, recipient_wallet_id, amount):
-        new_transaction = Transaction(sender_wallet_id=sender_wallet_id, recipient_wallet_id=recipient_wallet_id, amount=amount)
-        db.session.add(new_transaction)
-        db.session.commit()
-        return new_transaction
+
+        try:
+            if not sender_wallet_id or not recipient_wallet_id or amount <= 0:
+                raise ValueError("Invalid input parameters for creating a transaction")
+
+            new_transaction = Transaction(sender_wallet_id=sender_wallet_id, recipient_wallet_id=recipient_wallet_id, amount=amount)
+            db.session.add(new_transaction)
+            db.session.commit()
+
+            return new_transaction
+
+        except Exception as e:
+            db.session.rollback()
+            raise e
 
     @staticmethod
     def get_transactions_by_user(user_id):
-   
-        user_wallet = WalletService.get_wallet_by_user_id(user_id)
-        if user_wallet:
-            return Transaction.query.filter((Transaction.sender_wallet_id == user_wallet.id) | (Transaction.recipient_wallet_id == user_wallet.id)).all()
-        return []
+        try:
+            if not user_id:
+                raise ValueError("Invalid user_id for retrieving transactions")
 
+            user_wallet = WalletService.get_wallet_by_user_id(user_id)
+
+            if user_wallet:
+                return Transaction.query.filter((Transaction.sender_wallet_id == user_wallet.id) | (Transaction.recipient_wallet_id == user_wallet.id)).all()
+
+            return []
+
+        except Exception as e:
+            raise e
