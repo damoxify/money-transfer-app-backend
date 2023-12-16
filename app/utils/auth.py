9aef0ec5a9f0ac1f606f3ea96bb16c8bbe3c239f
Adedamola Ajasa <96835056+damoxify@users.noreply.github.com>
2023-12-16 19:20:48 +0100

    feat: Implement token generation and verification in auth.py

---
+++ app/utils/auth.py
@@ -0,0 +1,18 @@
+from flask import current_app, g, request
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired
+from app.models.user import User
+
+def generate_token(user_id, expiration=3600):
+    s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+    return s.dumps({'id': user_id}).decode('utf-8')
+
+def verify_token(token):
+    s = Serializer(current_app.config['SECRET_KEY'])
+    try:
+        data = s.loads(token)
+        user = User.query.get(data['id'])
+        g.current_user = user 
+        return True
+    except (BadSignature, SignatureExpired):
+        return False
+
