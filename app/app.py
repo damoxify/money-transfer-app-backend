5f641213e8f816e7406fb5731b48c969e972dd17
Adedamola Ajasa <96835056+damoxify@users.noreply.github.com>
2023-12-17 10:12:59 +0100

    feat: Initialize and configure Flask extensions

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- app/extensions.py
+++ app/extensions.py
@@ -2,9 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from app.app import app
 
 db = SQLAlchemy()
-migrate = Migrate()
+migrate = Migrate(app, db)
 cors = CORS()
 jwt = JWTManager()
 
