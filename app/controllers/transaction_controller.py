f132d3dfd9f838372bb189abc68ab473259bdcbd
Adedamola Ajasa <96835056+damoxify@users.noreply.github.com>
2023-12-21 09:40:27 +0100

    Update Controllers

--- app/controllers/transaction_controller.py
+++ app/controllers/transaction_controller.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_restx import Namespace, Resource
 from pagination.pagination import Pagination
 from models.transaction import Transaction
-
+from models.user import User
 
 transaction_bp = Blueprint('transaction', __name__)
 transaction_ns = Namespace('transaction', description='Transaction operations')
@@ -29,6 +29,32 @@ class ListTransactions(Resource):
             }
 
             return jsonify(response), 200
+
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+
+@transaction_ns.route('/user/<int:user_id>', methods=['GET'])
+class UserTransactions(Resource):
+    def get(self, user_id):
+        try:
+            user = User.query.get(user_id)
+
+            if user:
+                transactions_query = Transaction.query.filter_by(user_id=user.id)
+
+                paginated_transactions = pagination.paginate_query(transactions_query, 1, 10)
+
+                response = {
+                    'transactions': [{'id': transaction.id, 'amount': transaction.amount} for transaction in paginated_transactions['items']],
+                    'pagination': {
+                        'page': paginated_transactions['page'],
+                        'total_pages': paginated_transactions['total_pages'],
+                        'total_items': paginated_transactions['total_items'],
+                    }
+                }
+                return jsonify(response), 200
+            else:
+                return jsonify({'error': 'User not found'}), 404
 
         except Exception as e:
             return jsonify({'error': str(e)}), 500
--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -2,37 +2,59 @@ from flask import Blueprint, jsonify, request
 from flask_restx import Namespace, Resource
 from pagination.pagination import Pagination
 from models.user import User
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from models.transaction import Transaction
 
 user_bp = Blueprint('user', __name__)
 user_ns = Namespace('user', description='User operations')
 pagination = Pagination()
 
-@user_ns.route('/list', methods=['GET'])
-class ListUsers(Resource):
-    @jwt_required()  
-    def get(self):
+
+@user_ns.route('/profile/<int:user_id>', methods=['GET'])
+class UserProfile(Resource):
+    def get(self, user_id):
         try:
-            current_user = get_jwt_identity()
-            print(f"Authenticated user: {current_user}")
+            user = User.query.get(user_id)
 
-            page = request.args.get('page', default=1, type=int)
-            per_page = request.args.get('per_page', default=10, type=int)
-
-            users_query = User.query
-
-            paginated_users = pagination.paginate_query(users_query, page, per_page)
-
-            response = {
-                'users': [{'id': user.id, 'username': user.username} for user in paginated_users['items']],
-                'pagination': {
-                    'page': paginated_users['page'],
-                    'total_pages': paginated_users['total_pages'],
-                    'total_items': paginated_users['total_items'],
+            if user:
+                response = {
+                    'fullname': user.fullname,
+                    'date_of_birth': user.date_of_birth,
+                    'gender': user.gender,
+                    'bvn': user.bvn,
+                    'phone': user.phone,
+                    'address': user.address,
+                    'email': user.email 
                 }
-            }
+                return jsonify(response), 200
+            else:
+                return jsonify({'error': 'User not found'}), 404
 
-            return jsonify(response), 200
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+
+
+@user_ns.route('/transactions/<int:user_id>', methods=['GET'])
+class UserTransactions(Resource):
+    def get(self, user_id):
+        try:
+            user = User.query.get(user_id)
+
+            if user:
+                transactions_query = Transaction.query.filter_by(user_id=user.id)
+
+                paginated_transactions = pagination.paginate_query(transactions_query, 1, 10)
+
+                response = {
+                    'transactions': [{'id': transaction.id, 'amount': transaction.amount} for transaction in paginated_transactions['items']],
+                    'pagination': {
+                        'page': paginated_transactions['page'],
+                        'total_pages': paginated_transactions['total_pages'],
+                        'total_items': paginated_transactions['total_items'],
+                    }
+                }
+                return jsonify(response), 200
+            else:
+                return jsonify({'error': 'User not found'}), 404
 
         except Exception as e:
             return jsonify({'error': str(e)}), 500
